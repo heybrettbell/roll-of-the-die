be9a906ee82518805c433436a05cae5eb38a7870
heybrettbell <brett@grassrootsonline.ca>
2017-06-29 16:14:06 -0400

    Completed dice exercise

---
+++ dice.rb
@@ -0,0 +1,6 @@
+dice_one = Random.rand(1..6)
+dice_two = Random.rand(1..6)
+
+total_roll = (dice_one + dice_two)
+puts "Your rolled a #{dice_one} and a #{dice_two}."
+puts "Your total is #{total_roll}."
