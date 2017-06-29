0930040e0544f911d9050b882b0fac83eeb925bc
heybrettbell <brett@grassrootsonline.ca>
2017-06-29 16:25:05 -0400

    Doubles exercise complete

---
+++ doubles.rb
@@ -0,0 +1,9 @@
+dice_one = Random.rand(1..6)
+dice_two = Random.rand(1..6)
+
+total_roll = (dice_one + dice_two)
+puts "Your rolled a #{dice_one} and a #{dice_two}."
+if dice_one == dice_two
+  puts "Doubles!"
+end
+puts "Your total is #{total_roll}."
